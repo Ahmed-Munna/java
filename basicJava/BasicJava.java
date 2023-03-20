1c4cef43d4acf260b92cf9832db87ca12f2ebdb1
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-03-21 01:19:41 +0600

    basic java practice

---
+++ basicJava/BasicJava.java
@@ -0,0 +1,8 @@
+package basicJava;
+
+public class BasicJava {
+    
+    public static void main(String[] args) {
+        System.out.println("something");
+    }
+}
---
+++ basicJava/Product.java
@@ -0,0 +1,15 @@
+package basicJava;
+
+public class Product {
+    
+    public static void main(String[] args) {
+
+        System.out.println("id: 101");
+        System.out.println("title: iphone15");
+        System.out.println("1890 euro");
+        System.out.println("description: perfect product for best quality");
+        System.out.println("category: phone");
+
+    }
+
+}
